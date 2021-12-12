1be0c9d523f3d36a62b5b8f95a909728cc2e6339
TDCIAN <tdcian71@gmail.com>
2021-12-13 07:53:26 +0900

    UI 완성

--- GitHubRepository/RepositoryListCell.swift
+++ GitHubRepository/RepositoryListCell.swift
@@ -5,4 +5,26 @@
 //  Created by JeongminKim on 2021/12/13.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class RepositoryListCell: UITableViewCell {
+    var repository: String?
+    
+    let nameLabel = UILabel()
+    let descriptionLabel = UILabel()
+    let startImageView = UIImageView()
+    let startLabel = UILabel()
+    let languageLabel = UILabel()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        [
+            nameLabel, descriptionLabel,
+            startImageView, startLabel, languageLabel
+        ].forEach {
+            contentView.addSubview($0)
+        }
+    }
+}
